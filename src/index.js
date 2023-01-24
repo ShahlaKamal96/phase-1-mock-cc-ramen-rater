e1c88a1d41c4cadc702e3069b8ed0d90b9ab331b
ShahlaKamal96 <shahla.kamal19@gmail.com>
2023-01-23 21:03:50 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 // write your code here
+const menu = document.getElementById('ramen-menu');
+
+fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(ramen => {
+            const img = document.createElement('img');
+            img.src = ramen.image;
+            menu.appendChild(img);
+        });
+    });
+
+menu.addEventListener("click", (event) => {
+    if (event.target.tagName === "IMG") {
+
+        const detail = document.getElementById('ramen-detail');
+
+        const ramenId = event.target.getAttribute('data-id');
+
+        fetch(`http://localhost:3000/ramens/${ramenId}`)
+            .then(response => response.json())
+            .then(data => {
+                detail.querySelector('.detail-image').src = data.image;
+                detail.querySelector('.name').innerHTML = data.name;
+                detail.querySelector('.restaurant').innerHTML = data.restaurant;
+                document.getElementById('rating-display').innerHTML = data.rating;
+                document.getElementById('comment-display').innerHTML = data.comment;
+            });
+    }
+});
+
